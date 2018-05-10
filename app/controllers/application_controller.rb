b879ca5c77c3282364df7fb33243317d1cbcf8f3
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-10 15:50:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @team = Team.new(params[:team_name], params[:team_motto])
+      @team = Team.new(args)
 
       params[:team][:heroes].each do |details|
         binding.pry
